c5890932c1442a2e7e0993e974daeb310cb683c5
raghav94603 <raghavasawa13@gmail.com>
2021-04-18 12:20:42 +0530

    Second Commit

---
+++ functions.py
@@ -0,0 +1,21 @@
+from builtins import input
+
+a=int(input())
+b=int(input())
+
+def add() :
+    print(a+b)
+
+def sub():
+    print(a-b)
+
+def mul():
+    print(a*b)
+
+def div():
+    print(a/b)
+
+add()
+sub()
+mul()
+div()
