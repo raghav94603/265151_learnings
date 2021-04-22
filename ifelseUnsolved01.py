6cade70b2928a743752cc427b0725ae1c9afb247
raghav94603 <raghavasawa13@gmail.com>
2021-04-22 20:20:48 +0530

    unsolved01

---
+++ ifelseUnsolved01.py
@@ -0,0 +1,13 @@
+usr_str=input()
+if usr_str=='lol':
+    print("laughing out loud")
+elif usr_str=='rofl':
+    print("rolling on the floor laughing")
+
+elif usr_str=='lmk':
+    print("let me know")
+elif usr_str=='smh':
+    print("shaking my head")
+
+else:
+    print("You entered invalid input")
